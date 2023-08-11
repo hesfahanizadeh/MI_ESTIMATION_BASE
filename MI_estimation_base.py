46f969cf8d7f7b0a5d5e9f3d541b7b16c586b364
Homa Esfahanizadeh <50610987+hesfahanizadeh@users.noreply.github.com>
2023-08-11 15:43:06 -0400

    Update MI_estimation_base.py

--- MI_estimation_base.py
+++ MI_estimation_base.py
@@ -1,6 +1,6 @@
 # Title: Base framework for estimation of Mutual Information using Neural Networks
 # Last Update: 06/02/2023
-# Developers: William (Bill) Wu and Homa Esfahanizadeh
+# Developers: Homa Esfahanizadeh and William (Bill) Wu
 # Based on and Inspired by the public repositories of the following papers:
 #  I. Belghazi, S. Rajeswar, A.  Baratin, R. D. Hjelm, and A. C. Courville, “MINE: mutual information neural estimation,” PMLR, 2018.
 #  K. Choi and S. Lee, “Regularized mutual information neural estimation,” Openreview, 2021.
